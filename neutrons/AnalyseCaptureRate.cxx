6728e8815183ac744cc91f5ddb8cb668d7fb9f74
AranBorkum <anb28@sussex.ac.uk>
2020-02-26 04:20:54 -0600

    Making analyser agostic towards radio materials

--- neutrons/AnalyseCaptureRate.cxx
+++ neutrons/AnalyseCaptureRate.cxx
@@ -18,14 +18,19 @@ double cryoDimX[2] = {-947,  947};
 double cryoDimY[2] = {-887,  887};
 double cryoDimZ[2] = {-493, 6091};
 
+double APASpanX[2] = {-733.6944, 733.6944};
+double APASpanY[2] = {-606.0   , 606.0   };
+double APASpanZ[2] = {-443.8   , 5789.8  };
+
+
 std::string outFileDir = "/dune/app/users/aborkum/Neutrons/output/";
 
 bool isInCryostat(double x, double y, double z) {
 
   bool isWithin  = false;
-  bool isWithinX = (x<cryoDimX[1] && x>cryoDimX[0]);
-  bool isWithinY = (y<cryoDimY[1] && y>cryoDimY[0]);
-  bool isWithinZ = (z<cryoDimZ[1] && z>cryoDimZ[0]);
+  bool isWithinX = (x<APASpanX[1] && x>APASpanX[0]);
+  bool isWithinY = (y<APASpanY[1] && y>APASpanY[0]);
+  bool isWithinZ = (z<APASpanZ[1] && z>APASpanZ[0]);
 
   if (isWithinX && isWithinY && isWithinZ)
     isWithin = true;
@@ -62,7 +67,8 @@ int main(int argc, char** argv) {
   // --- Read in the input TFile and open the TTree
   FullGeoAnaInputManager *im = new FullGeoAnaInputManager();
   im->SetInputFile(InFileName);
-  im->SetInputTree("FullGeoAna"); /*im->SetInputTree("fullgeoana/FullGeoAna");*/
+  // im->SetInputTree("FullGeoAna"); 
+  im->SetInputTree("fullgeoana/FullGeoAna");
   im->LoadTree();
 
   int fNEvent = nEvent;
@@ -75,13 +81,17 @@ int main(int argc, char** argv) {
   std::vector<std::string> materials;
   std::vector<Capture*>     Captures;
 
-  TH1D     *neutronGenE = new TH1D    ("neutronGenE", "neutronGenE",  200, 0, 10);
-  TH1D     *gammaCapE   = new TH1D    ("gammaCapE"  , "gammaCapE"  , 1000, 0, 10);
-  TH1D     *endMaterial = new TH1D    ("endMaterial", "endMaterial",   11, 0, 11);
-  TH1D     *nGamma      = new TH1D    ("nGamma"     , "nGamma"     ,   15, 0, 15);
-  TH2D     *nCaptureXY  = new TH2D    ("nCaptureXY" , "nCaptureXY" ,  500, -1300,  1300,  500, -1500, 3000);
-  TH2D     *nCaptureYZ  = new TH2D    ("nCaptureYZ" , "nCaptureYZ" , 8000, -1000,  15000, 2000, -1300, 3000);
-  TProfile *capTypeE    = new TProfile("capTypeE"   , "capTypeE"   ,   11,     0,    11,    0,    20);
+  TH1D     *neutronGenE     = new TH1D    ("neutronGenE", "neutronGenE",  200, 0, 10);
+  TH1D     *gammaCapE       = new TH1D    ("gammaCapE"  , "gammaCapE"  , 1000, 0, 10);
+  TH1D     *endMaterial     = new TH1D    ("endMaterial", "endMaterial",   11, 0, 11);
+  TH1D     *nGamma          = new TH1D    ("nGamma"     , "nGamma"     ,   15, 0, 15);
+  TH2D     *nCaptureXY      = new TH2D    ("nCaptureXY" , "nCaptureXY" ,  500, -1300,  1300,  500, -1500, 3000);
+  TH2D     *nCaptureYZ      = new TH2D    ("nCaptureYZ" , "nCaptureYZ" , 2000, -1000,  7000, 2000, -1300, 3000);
+  TH2D     *nStartXY        = new TH2D    ("nStartXY"   , "nStartXY"   ,  500, -1300,  1300,  500, -1500, 3000);
+  TH2D     *nStartYZ        = new TH2D    ("nStartYZ"   , "nStartYZ"   , 2000, -1000,  7000, 2000, -1300, 3000);
+  TH2D     *nCaptureXYInLAr = new TH2D    ("nCaptureXYInLAr", "nCaptureXYInLAr", 100, -1000, 1000, 100, -1000, 1000);
+  TH2D     *nCaptureYZInLAr = new TH2D    ("nCaptureYZInLAr", "nCaptureYZInLAr", 200, -1000, 7000, 100, -1000, 1000);
+  TProfile *capTypeE        = new TProfile("capTypeE"   , "capTypeE"   ,   11,     0,    11,    0,    20);
 
   // --- Get list of all of the materials in the TTree
   for (int CurrentEvent=0; CurrentEvent<fNEvent; ++CurrentEvent) {
@@ -93,9 +103,12 @@ int main(int argc, char** argv) {
 
     for (auto const& it: *im->True_Bck_EndMaterial) {
       bool inVector = false;
+      std::string CurrentMaterial = it;
+      if (it.substr(0, 5) == "Radio")
+	CurrentMaterial = it.substr(5, -1);
       for (auto const itt: materials)
-	if (it == itt) inVector = true;
-      if (!inVector) materials.push_back(it);
+	if (CurrentMaterial == itt) inVector = true;
+      if (!inVector) materials.push_back(CurrentMaterial);
     }
   }
   std::cout << std::endl;
@@ -117,13 +130,22 @@ int main(int argc, char** argv) {
 
       // --- Fill the capture type histogram
       if (isCaptured && isNeutron)
-	for (size_t itt=0; itt<materials.size(); ++itt)
-	  if ((*im->True_Bck_EndMaterial)[it] == materials[itt])
+	for (size_t itt=0; itt<materials.size(); ++itt) {
+	  std::string WorkingMaterial = (*im->True_Bck_EndMaterial)[it];
+	  if ((*im->True_Bck_EndMaterial)[it].substr(0, 5) == "Radio")
+	    WorkingMaterial = (*im->True_Bck_EndMaterial)[it].substr(5, -1);
+	  if (WorkingMaterial == materials[itt])
 	    endMaterial->Fill(itt);
 
+	}
+
       // --- Count the number of primary and secondary neutrons
-      if (isNeutron && (*im->True_Bck_Mother)[it] == 0) 
-	{ nPrimaryNeutrons++; neutronGenE->Fill((*im->True_Bck_Energy)[it]*1000 - neutronMass); }
+      if (isNeutron && (*im->True_Bck_Mother)[it] == 0) { 
+	nPrimaryNeutrons++; 
+	neutronGenE->Fill((*im->True_Bck_Energy)[it]*1000 - neutronMass);
+	nStartXY->Fill((*im->True_Bck_VertX)[it], (*im->True_Bck_VertY)[it]);
+	nStartYZ->Fill((*im->True_Bck_VertZ)[it], (*im->True_Bck_VertY)[it]);
+      }
       if (isNeutron && (*im->True_Bck_Mother)[it] != 0) 
 	{ nSecondaryNeutrons++; }
 
@@ -134,11 +156,20 @@ int main(int argc, char** argv) {
 	    photons.push_back((*im->True_Bck_Energy)[itt]);
 
       // --- Count the number of captures on LAr
-      if (isNeutron && isCaptured && (*im->True_Bck_EndMaterial)[it] == "LAr")
+      if (isNeutron && isCaptured && (*im->True_Bck_EndMaterial)[it] == "LAr"
+	  && isInCryostat((*im->True_Bck_EndX)[it], 
+			  (*im->True_Bck_EndY)[it], 
+			  (*im->True_Bck_EndZ)[it]))
 	nCapturesOnLAr++;
 	    
       if (isNeutron && isCaptured) {
-	Capture *cap = new Capture((*im->True_Bck_EndMaterial)[it],
+	std::string thisMaterial;
+	if ((*im->True_Bck_EndMaterial)[it].substr(0, 5) == "Radio")
+	  thisMaterial = (*im->True_Bck_EndMaterial)[it].substr(5, -1);
+	else
+	  thisMaterial = (*im->True_Bck_EndMaterial)[it];
+	
+	Capture *cap = new Capture(thisMaterial,
 				   (*im->True_Bck_EndX)       [it],
 				   (*im->True_Bck_EndY)       [it],
 				   (*im->True_Bck_EndZ)       [it],
@@ -163,6 +194,12 @@ int main(int argc, char** argv) {
     nCaptureXY ->Fill(it->GetEndPointX(), it->GetEndPointY());
     nCaptureYZ ->Fill(it->GetEndPointZ(), it->GetEndPointY());
     
+    if (it->GetEndMaterial() == "LAr") {
+      std::cout << "This one" << std::endl;
+      nCaptureXYInLAr ->Fill(it->GetEndPointX(), it->GetEndPointY());
+      nCaptureYZInLAr ->Fill(it->GetEndPointZ(), it->GetEndPointY());
+    }
+
     for (size_t itt=0; itt<materials.size(); ++itt) 
       if (it->GetEndMaterial() == materials[itt])
 	capTypeE->Fill(itt,  it->GetSumPhotonEnergy());
@@ -186,6 +223,31 @@ int main(int argc, char** argv) {
   nCaptureYZ->GetXaxis()->SetTitle("Z Coordinate [cm]");
   nCaptureYZ->GetYaxis()->SetTitle("Y Coordinate [cm]");
   c->Print((outFileDir+OutFileName+".pdf").c_str());
+
+  nStartXY->Draw("COLZ");
+  nStartXY->SetStats(0);
+  nStartXY->GetXaxis()->SetTitle("X Coordinate [cm]");
+  nStartXY->GetYaxis()->SetTitle("Y Coordinate [cm]");
+  c->Print((outFileDir+OutFileName+".pdf").c_str());
+ 
+  nStartYZ->Draw("COLZ");
+  nStartYZ->SetStats(0);
+  nStartYZ->GetXaxis()->SetTitle("Z Coordinate [cm]");
+  nStartYZ->GetYaxis()->SetTitle("Y Coordinate [cm]");
+  c->Print((outFileDir+OutFileName+".pdf").c_str());
+
+  nCaptureXYInLAr->Draw("COLZ");
+  nCaptureXYInLAr->SetStats(0);
+  nCaptureXYInLAr->GetXaxis()->SetTitle("X Coordinate [cm]");
+  nCaptureXYInLAr->GetYaxis()->SetTitle("Y Coordinate [cm]");
+  c->Print((outFileDir+OutFileName+".pdf").c_str());
+ 
+  nCaptureYZInLAr->Draw("COLZ");
+  nCaptureYZInLAr->SetStats(0);
+  nCaptureYZInLAr->GetXaxis()->SetTitle("Z Coordinate [cm]");
+  nCaptureYZInLAr->GetYaxis()->SetTitle("Y Coordinate [cm]");
+  c->Print((outFileDir+OutFileName+".pdf").c_str());
+
 
   neutronGenE->Draw();
   neutronGenE->SetStats(0);
@@ -229,7 +291,7 @@ int main(int argc, char** argv) {
   std::cout << std::endl;
   std::cout << "Number of primary neutrons:        \t" << nPrimaryNeutrons << std::endl; 
   std::cout << "Number of captures on LAr:         \t" << nCapturesOnLAr   << std::endl;
-  std::cout << "Radiological neutron capture rate: \t" << (double)nCapturesOnLAr / ((double)nPrimaryNeutrons * 2 * 2.246e-3) 
+  std::cout << "Radiological neutron capture rate: \t" << (double)nCapturesOnLAr / ((double)fNEvent * 2 * 2.246e-3) 
 	    << " [Hz]" << std::endl;
 
   std::ofstream file;
@@ -239,7 +301,7 @@ int main(int argc, char** argv) {
   file << std::endl;
   file << "Number of primary neutrons:        \t" << nPrimaryNeutrons << std::endl; 
   file << "Number of captures on LAr:         \t" << nCapturesOnLAr   << std::endl;
-  file << "Radiological neutron capture rate: \t" << (double)nCapturesOnLAr / ((double)nPrimaryNeutrons * 2 * 2.246e-3) 
+  file << "Radiological neutron capture rate: \t" << (double)nCapturesOnLAr / ((double)fNEvent * 2 * 2.246e-3) 
        << " [Hz]" << std::endl;
   file.close();
 
